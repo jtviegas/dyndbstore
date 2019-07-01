b368e12ef318762f3291d3057d6037ead3c1a972
joao tiago viegas <joao.viegas@maersk.com>
2019-07-01 23:10:46 +0200

    tweaking the travis scripts

--- test.sh
+++ test.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 this_folder="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+if [ "$this_folder" -eq "" ]; then
+    this_folder=$(dirname $(readlink -f $0))
+fi
 parent_folder=$(dirname $this_folder)
 
 CONTAINER=dynamodb4test
@@ -12,6 +15,7 @@ docker run -d -p 8000:8000 --name $CONTAINER amazon/dynamodb-local
 _pwd=`pwd`
 cd $this_folder
 mocha --reporter spec
+echo "mocha outcopme: $?"
 cd $_pwd
 echo "...stopping db container..."
 docker stop $CONTAINER && docker rm $CONTAINER
