c2fa988c2300d0c0aebe8d965476f026fdd7da35
joao tiago viegas <jtgv@novonordisk.com>
2025-02-08 17:50:53 +0100

    wip

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   testEnvironment: 'node',
   collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
   roots: ['<rootDir>/test'],
   testMatch: ['**/*.test.js'],
 };
